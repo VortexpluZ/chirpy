2038e767aa608d1ff2419bfbf1961f3ddd29b1e8
VortexpluZ <j.garzagonzalez@celonis.com>
2026-07-08 18:40:32 +0200

    replace value for pointer reference

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
-	server := http.Server{Handler: mux, Addr: ":8080"}
+	server := &http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
