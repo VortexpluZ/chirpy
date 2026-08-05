c0b0dcedc27bd866738b540a2dab0a6c29ef243b
VortexpluZ <j.garzagonzalez@celonis.com>
2026-08-05 22:26:07 +0200

    swap metrics and rest from api to admin

--- main.go
+++ main.go
@@ -19,9 +19,14 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) getMetrics() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(200)
-		w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+		w.Write([]byte(fmt.Sprintf(`<html>
+		<body>
+		  <h1>Welcome, Chirpy Admin</h1>
+		  <p>Chirpy has been visited %d times!</p>
+		</body>
+	  </html>`, cfg.fileserverHits.Load())))
 	})
 }
 
@@ -40,8 +45,8 @@ func main() {
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
-	mux.Handle("GET /api/metrics", config.getMetrics())
-	mux.Handle("POST /api/reset", config.resetMetrics())
+	mux.Handle("GET /admin/metrics", config.getMetrics())
+	mux.Handle("POST /admin/reset", config.resetMetrics())
 	server := &http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
