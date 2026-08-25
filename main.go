4dd7588e0e02c57a3edfa015fa64e0834e5e4331
VortexpluZ <j.garzagonzalez@celonis.com>
2026-08-25 15:44:07 +0200

    simplify respond with Error func

--- main.go
+++ main.go
@@ -53,17 +53,9 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	type returnVals struct {
 		Error string `json:"error"`
 	}
-	respBody := returnVals{
+	respondWithJSON(w, code, returnVals{
 		Error: msg,
-	}
-	dat, err := json.Marshal(respBody)
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(dat)
+	})
 }
 
 func capitalizeProfane(s string) string {
