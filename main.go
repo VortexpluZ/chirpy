627c554d9075e6d21209e7e3ce85985a0079e032
VortexpluZ <j.garzagonzalez@celonis.com>
2026-08-28 22:18:34 +0200

    new login endpoint + update createUser endpoint

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/VortexpluZ/chirpy/internal/auth"
 	"github.com/VortexpluZ/chirpy/internal/database"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
@@ -225,23 +226,81 @@ func healthz() http.Handler {
 func (cfg *apiConfig) createUser() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		type parameters struct {
-			Email string `json:"email"`
+			Email    string `json:"email"`
+			Password string `json:"password"`
 		}
 		decoder := json.NewDecoder(r.Body)
 		params := parameters{}
 		err := decoder.Decode(&params)
 		if err != nil {
-			respondWithError(w, 500, "Something went wrong")
+			respondWithError(w, 400, "Bad Request")
+			log.Println(err)
 			return
 		}
 
-		user, err := cfg.database.CreateUser(r.Context(), params.Email)
+		hashed_password, err := auth.HashPassword(params.Password)
 		if err != nil {
 			respondWithError(w, 500, "Something went wrong")
+			log.Println(err)
+			return
+		}
+		user, err := cfg.database.CreateUser(r.Context(),
+			database.CreateUserParams{
+				Email:          params.Email,
+				HashedPassword: hashed_password,
+			})
+
+		if err != nil {
+			respondWithError(w, 500, "Something went wrong")
+			log.Println(err)
 			return
 		}
 
 		respondWithJSON(w, 201, User{
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			ID:        user.ID,
+			Email:     user.Email,
+		})
+	})
+}
+
+func (cfg *apiConfig) login() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		}
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			respondWithError(w, 400, "Bad Request")
+			log.Println(err)
+			return
+		}
+
+		user, err := cfg.database.GetUserByEmail(r.Context(), params.Email)
+		if err != nil {
+			respondWithError(w, 500, "Something went wrong")
+			log.Println(err)
+			return
+		}
+
+		match, err := auth.CheckPasswordHash(params.Password, user.HashedPassword)
+		if err != nil {
+			respondWithError(w, 500, "Something went wrong")
+			log.Println(err)
+			return
+		}
+
+		if !match {
+			respondWithError(w, 401, "Unauthorized")
+			log.Println(err)
+			return
+		}
+
+		respondWithJSON(w, 200, User{
 			CreatedAt: user.CreatedAt,
 			UpdatedAt: user.UpdatedAt,
 			ID:        user.ID,
@@ -265,6 +324,7 @@ func main() {
 	mux.Handle("/app/", http.StripPrefix("/app", config.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.Handle("GET /api/healthz", healthz())
 	mux.Handle("POST /api/users", config.createUser())
+	mux.Handle("POST /api/login", config.login())
 	mux.Handle("POST /api/chirps", config.createChirp())
 	mux.Handle("GET /api/chirps", config.getChirps())
 	mux.Handle("GET /api/chirps/{chirpID}", config.getChirp())
